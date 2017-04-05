e31bc265c5bc2e8900cef1d5e788c3539330c16a
Atul <kkmonlee@gmail.com>
2017-04-05 18:31:09 +0100

    Added date/time value

--- add_data.php
+++ add_data.php
@@ -1,9 +1,14 @@
 <?php
 	include("dbconnect.php");
 
-	$SQL = "INSERT INTO kkmonlee_seatspace.tables (id, taken, time) VALUES ('".$_GET["id"]."', '".$_GET["taken"]."', '".$_GET["time"]."')";
+	//$SQL = "INSERT INTO kkmonlee_seatspace.Floor3 (id, taken, time) VALUES ('".$_GET["id"]."', '".$_GET["taken"]."', '".$_GET["time"]."')";
+	date_default_timezone_set("Europe/London");
+
+	$date = date('Y-m-d H:i:s');
+
+	$SQL = "UPDATE kkmonlee_seatspace.Floor3 SET taken = '".$_GET["taken"]."', time = '$date' WHERE id = '".$_GET["id"]."'";
 
 	mysql_query($SQL);
 
-	header("Location: review_data.php");
+	header("Location: review_data.php");s
  ?>
